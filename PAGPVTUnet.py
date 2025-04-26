0e00ecd753ecb7456468139c497436bc8e28e541
faresbougourzi <faresbougourzi@gmail.com>
2025-04-26 16:48:55 +0200

    Update and rename PAG_TransYnet.py to PAGPVTUnet.py

--- PAGPVTUnet.py
+++ PAGPVTUnet.py
@@ -215,10 +215,10 @@ class Conv2dReLU(nn.Sequential):
         super(Conv2dReLU, self).__init__(conv, bn, relu) 
         
         
-#### PAGTransYnet #########################################################  
-class PAGTransYnet(nn.Module):
+#### PAG-PVTUnet #########################################################  
+class PAGPVTUnet(nn.Module):
     def __init__(self, input_channels=3, num_classes = 1, deep_supervision=False):
-        super(PAGTransYnet, self).__init__()
+        super(PAGPVTUnet, self).__init__()
         
         self.pool = nn.MaxPool2d(2, 2)
         self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
